2d2c9a8a795a3d104fb138833e93b3900d105c2d
M Fakhri Aldin <mfakhri.aldin@gmai.com>
2021-10-02 10:10:37 +0700

    codingtest

---
+++ fakhri/minggu2.py
@@ -0,0 +1,13 @@
+import statistics
+
+#bingung input kalo ada str sama int dalam satu list kak
+
+# x=[]
+# for i in range(int(input)):
+#     x.append(int(input()))
+
+
+# list = [19,21, 36]
+# avg = statistics.mean(list)
+# print(round(avg,2))
+
