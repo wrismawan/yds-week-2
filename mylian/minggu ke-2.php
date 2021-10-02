ad71333e7bf4633202d264140d9c95c4a8e32311
M Fakhri Aldin <mfakhri.aldin@gmai.com>
2021-10-02 10:45:57 +0700

    add comment on logic

--- mylian/minggu ke-2.php
+++ mylian/minggu ke-2.php
@@ -5,10 +5,11 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Quiz Bonus</title>
-
+  <!-- menghubungkan dengan bootstrap -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 </head>
 <body>
+  <!-- membuat User interface untuk input -->
   <div class="container py-5">
     <div class="row">
       <div class="col-lg-12">
@@ -33,7 +34,7 @@
         </div>
       </div>
     </div>
-
+    <!-- membuat user interface untuk output -->
     <div class="row mt-4">
       <div class="col-lg-12">
         <div class="card">
@@ -41,7 +42,9 @@
             <h5>Output</h5>
           </div>
           <div class="card-body">
+            <!-- membuat program dengan php -->
             <?php
+              // wah ini saya kurang tau, sepertinya berhubungan dengan back end
               if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 $hasil = "";
                 $bil = $_POST["bil"];
@@ -55,7 +58,7 @@
                   <?php
                 }else{
                   $siswa_nilai = explode(" ",$siswa);
-                  
+                  // mengisi list
                   for($i = 1; $i < count($siswa_nilai); $i++){
                     if($i == count($siswa_nilai)-1){
                       echo "mohon maaf kak";
@@ -81,7 +84,7 @@
   </div>
 	<br>
 
-
+<!-- menambahkan file javascript -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 </body>
