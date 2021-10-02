ebf6f6b81d8aeeb39e109a81a51a7e283253d349
Yumna Pratista <yumna.tastaftian@pinhome.id>
2021-10-02 10:26:56 +0700

    add comments on logic

--- Wahyu/bonus-assignment-week2.py
+++ Wahyu/bonus-assignment-week2.py
@@ -1,18 +1,23 @@
 if __name__ == '__main__':
     
+    # meminta input jumlah siswa
     n = int(input())
 
+    # menyimpan nama dan nilai dari setiap siswa
     student_marks = {}
     for _ in range(n):
         name, *line = input().split()
         scores = list(map(float, line))
         student_marks[name] = scores
-        
+    
+    # meminta input nama siswa yang ingin dihitung rata2nya
     query_name = input()
     
+    # menjumlahkan nilai siswa
     sum = 0
     for score in student_marks[query_name]:
         sum += score
     
+    # mengeluarkan nilai rata2
     percentage = sum/len(student_marks[query_name])
     print("{:.2f}".format(percentage))
